2553b174b398cd877c4b6cb2369fdf3abc63a18c
pry-web <jhunildagasdas@gmail.com>
2026-08-12 17:27:14 +0800

    Update web.config.js

--- web.config.js
+++ web.config.js
@@ -1,4 +1,4 @@
 // Public runtime configuration. Do not put passwords or API secrets in this file.
 window.PRY_CONFIG = {
-  apiUrl: 'https://YOUR-WORKER.YOUR-SUBDOMAIN.workers.dev',
+  apiUrl: 'https://pry-attendance-api.jhunildagasdas.workers.dev',
 };
